4262b074453d7b3f17a6b9c8afabe9c0b2ffe885
CamiAlderete <49692059+CamiAlderete@users.noreply.github.com>
2022-03-12 21:01:30 -0300

    proyecto base

---
+++ src/main/java/py/com/progweb/prueba/model/Persona.java
@@ -0,0 +1,62 @@
+package py.com.progweb.prueba.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="persona")
+public class Persona {
+
+    @Id
+    @Column(name="id_persona")
+    @Basic(optional = false)
+    @GeneratedValue(generator = "personaSeq", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "persona_seq",sequenceName = "personaSeq", allocationSize = 0)
+    private Integer id;
+
+    @Column(name="nombre", length = 50)
+    @Basic(optional = false)
+    private String nombre;
+
+    @Column(name="apellido", length = 50)
+    @Basic(optional = false)
+    private String apellido;
+
+    public Persona(Integer id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                '}';
+    }
+}
