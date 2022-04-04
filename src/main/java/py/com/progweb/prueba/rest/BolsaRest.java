050dc0a7b86d46a1619bd312eef9c91cd60ebc6c
CamiAlderete <49692059+CamiAlderete@users.noreply.github.com>
2022-04-04 11:23:04 -0400

    faltante get bolsa by cliente y por rango

--- src/main/java/py/com/progweb/prueba/rest/BolsaRest.java
+++ src/main/java/py/com/progweb/prueba/rest/BolsaRest.java
@@ -1,11 +1,14 @@
 package py.com.progweb.prueba.rest;
 
 import py.com.progweb.prueba.ejb.BolsaDAO;
+import py.com.progweb.prueba.ejb.ClienteDAO;
 import py.com.progweb.prueba.model.Bolsa;
+import py.com.progweb.prueba.model.Cliente;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("bolsa")
 @Consumes("application/json")
@@ -14,6 +17,9 @@ import javax.ws.rs.core.Response;
 public class BolsaRest {
     @Inject
     private BolsaDAO bolsaDAO;
+
+    @Inject
+    private ClienteDAO clienteDAO;
 
     @GET
     @Path("/")
@@ -65,5 +71,34 @@ public class BolsaRest {
             return Response.serverError().build();
         }
     }
+
+    @GET
+    @Path("/cliente/{id}")
+    public Response getByCliente( @PathParam("id") int id ){
+        try {
+
+            Cliente cliente = clienteDAO.get(id);
+            List<Bolsa> lista = bolsaDAO.getBolsasPorCliente(cliente);
+            return Response.ok( lista ).build();
+
+        }catch(Exception e){
+            return Response.serverError().build();
+        }
+    }
+
+    @GET
+    @Path("/rango/{li}/{ls}")
+    public Response getByRango( @PathParam("li") int li, @PathParam("ls") int ls ){
+        try {
+
+            List<Bolsa> lista = bolsaDAO.getBolsasPorRango(li,ls);
+            return Response.ok( lista ).build();
+
+        }catch(Exception e){
+            return Response.serverError().build();
+        }
+    }
+
+
 
 }
